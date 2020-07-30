b810fe2e7861a9402f1d239fc3d4b4a6fa45bde1
vasiinso <vasiinso@gmail.com>
2020-07-31 02:51:09 +0530

    Update install_thrift.sh

--- scripts/install_thrift.sh
+++ scripts/install_thrift.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update -qq
 sudo apt-get install libboost-dev libboost-test-dev libboost-program-options-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev
-wget http://www.us.apache.org/dist/thrift/0.11.0/thrift-0.11.0.tar.gz
-tar xfz thrift-0.11.0.tar.gz
-cd thrift-0.\11.0 && ./configure --without-ruby --without-tests && sudo make install
+wget http://www.us.apache.org/dist/thrift/0.10.0/thrift-0.10.0.tar.gz
+tar xfz thrift-0.10.0.tar.gz
+cd thrift-0.10.0 && ./configure --without-ruby --without-tests && sudo make install
 
